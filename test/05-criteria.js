9cca99dcc7c6fe84760941c8e36512e5cf13c181
Jelle De Loecker <jelle@develry.be>
2019-01-18 16:03:14 +0100

    Change the way Criteria selects fields & associations

--- test/05-criteria.js
+++ test/05-criteria.js
@@ -11,6 +11,71 @@ describe('Criteria', function() {
 		Person = Model.get('Person');
 	});
 
+	describe('#select(field)', function() {
+		it('selects the given field', async function() {
+
+			var criteria = Person.find();
+
+			criteria.select('firstname');
+
+			let records = await Person.find(criteria);
+
+			assert.strictEqual(records.length, 2);
+
+			let record = records[0];
+
+			assert.strictEqual(!!record._id, true, 'The _id should always be selected');
+			assert.strictEqual(typeof record.firstname, 'string');
+			assert.strictEqual(typeof record.lastname, 'undefined');
+			assert.strictEqual(typeof record.birthdate, 'undefined');
+			assert.strictEqual(typeof record.male, 'undefined');
+			assert.strictEqual(typeof record.Parent, 'undefined');
+			assert.strictEqual(typeof records[1].Parent, 'undefined');
+		});
+	});
+
+	describe('#select(association)', function() {
+		it('also query the association', async function() {
+
+			var criteria = Person.find();
+
+			criteria.select('Parent');
+			criteria.where('firstname').equals('Jelle');
+
+			let records = await Person.find(criteria);
+
+			assert.strictEqual(records.length, 1);
+
+			let record = records[0];
+
+			assert.strictEqual(!!record.Parent, true, 'The Parent record wasn\'t selected');
+			assert.strictEqual(record.Parent.firstname, 'Griet', 'The wrong record was selected as Parent');
+			assert.strictEqual(typeof record.Parent.lastname, 'string', 'The lastname field was not selected');
+			assert.strictEqual(record.Parent.lastname, 'De Leener');
+		});
+	});
+
+	describe('#select(association.field)', function() {
+		it('also query the association but limit it to the given field', async function() {
+
+			var criteria = Person.find();
+
+			criteria.select('Parent.firstname');
+			criteria.where('firstname').equals('Jelle');
+
+			let records = await Person.find(criteria);
+
+			assert.strictEqual(records.length, 1);
+
+			let record = records[0];
+
+			assert.strictEqual(!!record.Parent, true, 'The Parent record wasn\'t selected');
+			assert.strictEqual(!!record.Parent._id, true, 'The Parent should have an _id field');
+			assert.strictEqual(record.Parent.firstname, 'Griet', 'The wrong record was selected as Parent');
+			assert.strictEqual(typeof record.Parent.lastname, 'undefined', 'The parent record should only have an _id and firstname field');
+		});
+	});
+
 	describe('#where(field)', function() {
 		it('targets the given field', async function() {
 
